7f41093fe8ae4bd81d031ccdb4fa9f45202ef07d
Siddharth Kundu <sk31395@gmail.com>
2018-11-23 00:41:07 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+// allow cross-origin requests
+app.use(cors())
 
 mongoose.connect(
   'mongodb://test:test123@ds121289.mlab.com:21289/react-graphql',
